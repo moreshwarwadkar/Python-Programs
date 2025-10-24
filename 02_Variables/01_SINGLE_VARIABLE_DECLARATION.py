0c5df541890963f63cd753e48283d73a8b580eee
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-10-24 13:18:50 +0530

    Add files via upload

---
+++ 02_Variables/01_SINGLE_VARIABLE_DECLARATION.py
@@ -0,0 +1,4 @@
+# SINGLE VARIABLE DECLARATION
+
+a = 10 # int
+s = 'UNiK' #string
---
+++ 02_Variables/02_MULTIPLE_VARIABLE_DECLARATION.py
@@ -0,0 +1,5 @@
+# MULTIPLE VARIABLE DECLARATION
+
+a, b, c = 10, 20, 30
+
+# Note: The Number of variables should be similar to the number of values.
