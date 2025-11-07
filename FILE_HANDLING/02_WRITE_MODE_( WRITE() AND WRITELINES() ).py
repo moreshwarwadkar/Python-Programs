1d21ddddffcfac35b9ee3efe77a5c6bd28fb3d96
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-11-07 18:56:47 +0530

    Delete FILE_HANDLING directory

--- FILE_HANDLING/02_WRITE_MODE_( WRITE() AND WRITELINES() ).py
+++
@@ -1,6 +0,0 @@
-# WRITE MODE ( WRITE() AND WRITELINES() FUNCTION )..
-
-a = open('2_File.txt','w')
-a.write('Good Morning..!!')
-a.writelines(['Hello..', 'Now I Am a Billionaire', 'Also, I Am Successfull Young Trader'])
-a.close()
