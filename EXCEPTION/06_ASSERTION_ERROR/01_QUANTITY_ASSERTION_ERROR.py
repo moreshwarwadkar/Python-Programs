57406f85a1ad940564bc9bfaa335cd6a151b16a3
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-11-15 22:51:40 +0530

    Delete EXCEPTION directory

--- EXCEPTION/06_ASSERTION_ERROR/01_QUANTITY_ASSERTION_ERROR.py
+++
@@ -1,6 +0,0 @@
-stock = 10
-
-qty = int(input('Enter The Quantity:'))
-
-assert qty <= stock , 'Out Of Stock'
-print('Order Placed')
--- EXCEPTION/06_ASSERTION_ERROR/01_VOTING_.py
+++
@@ -1,5 +0,0 @@
-age = int(input('Enter Age:'))
-
-
-assert age >= 18, 'You Are Not Eligible'
-print('You Are Eligible For Vote')
