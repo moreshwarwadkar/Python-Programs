2e0fee5ea4cb29e551e5276c0a5cf38717e10feb
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-10 12:33:31 +0530

    Update 03_PRIVATE_ACCESS_SPECIFIER.py

--- 15_OOP'S/07_ENCAPSULATION/#_ACCESS_SPECIFIERS/03_PRIVATE_ACCESS_SPECIFIER.py
+++ 15_OOP'S/07_ENCAPSULATION/#_ACCESS_SPECIFIERS/03_PRIVATE_ACCESS_SPECIFIER.py
@@ -21,6 +21,56 @@ obj.display()
 
 
 
+# EXAMPLE 2:
+
+class a:
+    
+    __name = 'UNiK'
+    
+    def __init__(self):
+        
+        self.__num = 0
+        
+    def dis(self):
+        print(self.__num)
+        
+    def mod_num(self,n):
+        self.__num = n
+
+    @classmethod
+    def d_cls(cls):
+        print(cls.__name)
+        
+    @classmethod
+    def __dis_cls(cls):
+        print(cls.__name)
+        
+    @classmethod
+    def mod_name(cls,n):
+        cls.__name = n
+        
+obj = a()
+
+obj.mod_num(100)
+obj.dis() # 100
+
+obj.d_cls() # UNiK
+obj.mod_name('Rohan') 
+obj.d_cls() # Rohan
+
+print(obj._a__num) # 100
+# print(obj.a__name) -> It Will Through The Error..
+print(a._a__name) # Rohan
+
+
+obj._a__dis_cls()  # Works only if method starts with __  : Rohan
+# obj._a__dis() -> It Will Through The Error..
+
+# =============================================
+
+
+
+
 
 # EXAMPLE 2:
 
