d99b113b3f1705e6c5a11285d97efdd4031456ed
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-10 12:57:22 +0530

    Update 03_PRIVATE_ACCESS_SPECIFIER.py

--- 15_OOP'S/07_ENCAPSULATION/#_ACCESS_SPECIFIERS/03_PRIVATE_ACCESS_SPECIFIER.py
+++ 15_OOP'S/07_ENCAPSULATION/#_ACCESS_SPECIFIERS/03_PRIVATE_ACCESS_SPECIFIER.py
@@ -72,7 +72,7 @@ obj._a__dis_cls()  # Works only if method starts with __  : Rohan
 
 
 
-# EXAMPLE 2:
+# EXAMPLE 3:
 
 class Bank:
     def __init__(self):
@@ -95,3 +95,53 @@ b.show()
 # OP: 500
 
 # =============================================
+
+
+
+
+# EXAMPLE 4:
+
+class BankAccount:
+    
+    def __init__(self):
+        self.__balance = 0
+        self.__account_holder = input('Enter Name:')
+        
+    def deposit(self):
+        
+        amt = int(input('Deposit Amount:'))
+        self.__balance = self.__balance+amt
+        print(f'{self.__balance} Amount Added Successfully')
+
+    def withdraw(self):
+        
+        amt = int(input('Withdraw Amount:'))
+        
+        if self.__balance != 0:
+            self.__balance = self.__balance - amt
+            print(f'{self.__balance} Amount Withdraw Successfully')
+        
+        else:
+            
+            print('Add Amount First')
+            
+    def show_balance(self):
+        print('Current Balance:',self.__balance)
+        
+    def __secret_message(self):
+        return 'This is Black Mony..!'
+        
+    def reveal_secret(self):
+        msg = self.__secret_message()
+        return msg
+    
+obj = BankAccount()
+
+obj.deposit()
+obj.withdraw()
+obj.show_balance()
+
+print(obj._BankAccount__balance)
+print(obj._BankAccount__secret_message())  # This is Black Mony..!
+
+print(obj.reveal_secret()) # This is Black Mony..!
