f6ed12c629c01648c1f2a59fad001b5f40d766b0
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-12 12:16:45 +0530

    Create 03_Without_Constructor.py

---
+++ 15_OOP'S/01_SIMPLE_PROGRAM/03_Without_Constructor.py
@@ -0,0 +1,10 @@
+class Book:
+        
+    def details(self):
+        print('This is a Book..')
+
+obj1 = Book()
+obj1.details() # This is a Book..
+
+obj2 = Book()
+obj2.details() # This is a Book..
