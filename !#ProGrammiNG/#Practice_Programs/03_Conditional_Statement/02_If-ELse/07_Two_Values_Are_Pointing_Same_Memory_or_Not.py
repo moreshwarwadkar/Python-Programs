4319feb8b553afa94d8a3262a7a7d62a4a0aaa0d
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-16 19:33:03 +0530

    Create 07_Two_Values_Are_Pointing_Same_Memory_or_Not.py

---
+++ !#ProGrammiNG/#Practice_Programs/03_Conditional_Statement/02_If-ELse/07_Two_Values_Are_Pointing_Same_Memory_or_Not.py
@@ -0,0 +1,10 @@
+# Wap to check whether 2 values are pointing to the same memory or not.
+
+val1 = eval(input('Enter Value 1:'))
+val2 = eval(input('Enter Value 2:'))
+
+if val1 is val2:
+    print('Values are Pointing Same Memory')
+
+else:
+    print('Values are Not Pointing Same Memory')
