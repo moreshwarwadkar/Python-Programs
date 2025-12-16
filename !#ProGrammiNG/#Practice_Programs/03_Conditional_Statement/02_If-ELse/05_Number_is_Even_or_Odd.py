380d3bfbb211f82484f227b58b066788d4ea387a
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-16 19:28:56 +0530

    Create 05_Number_is_Even_or_Odd.py

---
+++ !#ProGrammiNG/#Practice_Programs/03_Conditional_Statement/02_If-ELse/05_Number_is_Even_or_Odd.py
@@ -0,0 +1,9 @@
+# Wap to check whether the number is even or odd.
+
+num = int(input('Enter Any Number:'))
+
+if num%2 == 0:
+    print('Given Number is Even')
+
+else:
+    print('Given Number is Odd')
