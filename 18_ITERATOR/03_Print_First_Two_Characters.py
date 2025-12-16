2e1af11ba9416cc549ecb3946d48e2d66bfd561b
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-16 08:33:47 +0530

    Create 03_Print_First_Two_Characters.py

---
+++ 18_ITERATOR/03_Print_First_Two_Characters.py
@@ -0,0 +1,16 @@
+# Create an iterator from a string and print its first two characters using iter() and next().
+
+s = 'SumiT'
+
+a = iter(s)
+
+print(next(a))
+print(next(a))
+
+'''
+OUTPUT :
+
+S
+u
+
+'''
