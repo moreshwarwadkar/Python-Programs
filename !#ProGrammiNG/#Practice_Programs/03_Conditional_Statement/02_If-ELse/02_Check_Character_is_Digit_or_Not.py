a45519062c79a276f9394a2e27fa6394f8fe85d8
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-16 17:48:13 +0530

    Create 02_Check_Character_is_Digit_or_Not.py

---
+++ !#ProGrammiNG/#Practice_Programs/03_Conditional_Statement/02_If-ELse/02_Check_Character_is_Digit_or_Not.py
@@ -0,0 +1,9 @@
+# Wap to check whether the given character is digit or not.
+
+ch = eval(input('Enter Data:'))
+
+if isinstance(ch,(float,int)):
+    print('Given Character is Digit')
+
+else:
+    print('Given Character is Not Digit')
