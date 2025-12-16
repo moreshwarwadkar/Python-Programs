a1a3153f6583dcad834349cd00eb31ae93a4fce5
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-16 19:40:56 +0530

    Create 01_Positve_or_Negative_Numbers.py

---
+++ !#ProGrammiNG/#Practice_Programs/03_Conditional_Statement/02_If-ELse/01_Positve_or_Negative_Numbers.py
@@ -0,0 +1,9 @@
+# Wap to check whether the number is positive or negative.
+
+num = int(input('Enter Any Number:'))
+
+if num>=0:
+    print('Number is Positive')
+    
+else:
+    print('Number is Negative')
