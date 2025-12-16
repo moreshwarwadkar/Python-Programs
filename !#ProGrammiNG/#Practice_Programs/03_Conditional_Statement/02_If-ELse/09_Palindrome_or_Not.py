55e6fabc93fab64a628d35af3c9784b9e9151d6c
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-16 19:39:36 +0530

    Create 09_Palindrome_or_Not.py

---
+++ !#ProGrammiNG/#Practice_Programs/03_Conditional_Statement/02_If-ELse/09_Palindrome_or_Not.py
@@ -0,0 +1,11 @@
+# Wap to check whether the string is palindrome or not.
+
+s = 'madam'
+
+if s == s[::-1]:
+    print('Given String is Palindrome')
+    
+else:
+    print('Given String is Not a Palindrome')
+
+# Also we can use Multiple Ways..
