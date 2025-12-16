6045ba76c79e2c3e8e5cba6fdb221f0de703d6b0
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-16 20:11:54 +0530

    Create 06_Check_Relation_Between_Two_Numbers.py

---
+++ !#ProGrammiNG/#Practice_Programs/03_Conditional_Statement/03_Elif/06_Check_Relation_Between_Two_Numbers.py
@@ -0,0 +1,13 @@
+# Wap to check the relation between two integer numbers.
+
+num1 = int(input('Enter Number 1:'))
+num2 = int(input('Enter Number 2:'))
+
+if num1 > num2:
+    print('Number 1 is Greater than Number 2')
+    
+elif num1 < num2:
+    print('Number 1 is Smaller than Number 2')
+
+else:
+    print('Both Are Equal')
