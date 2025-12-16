4429ff86184ddec39f8ea7676369c6c6c0925d96
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-16 19:26:57 +0530

    Create 04_List_Consists_With_Middle_Value_or_Not.py

---
+++ !#ProGrammiNG/#Practice_Programs/03_Conditional_Statement/02_If-ELse/04_List_Consists_With_Middle_Value_or_Not.py
@@ -0,0 +1,9 @@
+# Wap to check whether a list consists of middle value or not.
+
+li  = [1,2,3,4,5]
+
+if len(li)%2 != 0:
+    print('List Consists of Middle Value')
+    
+else:
+    print('List Does Not Consists of Middle Value')
