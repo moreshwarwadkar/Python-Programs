8ae3bdff5aca897f20b9c57aa103360c617c71fd
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-16 20:45:17 +0530

    Create 03_Vowel_or_Consonent.py

---
+++ !#ProGrammiNG/#Practice_Programs/03_Conditional_Statement/04_Nested_If/03_Vowel_or_Consonent.py
@@ -0,0 +1,9 @@
+# Wap to check whether the character is vowel or consonant.
+
+ch = input('Enter Any Character:')
+
+if ch in 'aeiouAEIOU':
+    print('Given Character is Vowel')
+    
+else:
+    print('Given Character is Consonent')
