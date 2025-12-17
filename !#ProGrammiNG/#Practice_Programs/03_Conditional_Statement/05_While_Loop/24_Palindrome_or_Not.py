65fea8b83c2f74b7070a0b5f5e5b4ba4649ae527
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-12-17 10:21:46 +0530

    Create 24_Palindrome_or_Not.py

---
+++ !#ProGrammiNG/#Practice_Programs/03_Conditional_Statement/05_While_Loop/24_Palindrome_or_Not.py
@@ -0,0 +1,17 @@
+# Wap to check whether the number is palindrome or not.
+
+num  = int(input('Enter Number:'))
+temp = num
+rev = 0
+
+while num > 0:
+    
+    rem = num % 10
+    rev = rev*10+rem
+    num = num // 10
+    
+if temp == rev:
+    print('Number is Palindrome')
+
+else:
+    print('Number is Not Palindrome')
