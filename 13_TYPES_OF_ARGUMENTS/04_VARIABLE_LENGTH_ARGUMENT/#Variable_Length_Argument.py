ea381b073079f07831eeb232f851ff846ea6702c
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2026-02-09 19:43:23 +0530

    Create #Variable_Length_Argument.py

---
+++ 13_TYPES_OF_ARGUMENTS/04_VARIABLE_LENGTH_ARGUMENT/#Variable_Length_Argument.py
@@ -0,0 +1,11 @@
+# VARIABLE LENGTH ARGUMENT
+
+'''
+Variable Length Argument is also known as 'Arbitrary arguments'.
+Arbitrary arguments means : A function can accept any number of arguments (no fixed limit).
+
+
+We Have Two Types :
+--> 1) Single Packing ( Tuple Packing )
+--> 2) Double Packing ( Dictionary Packing )
+'''
