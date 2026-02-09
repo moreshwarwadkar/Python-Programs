ac2971eb9522931db06437d1b3f705e6f380feaa
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2026-02-09 18:25:01 +0530

    Update 01_INPUT_STATEMENTS.py

--- 08_INPUT_STATEMENT/01_INPUT_STATEMENTS.py
+++ 08_INPUT_STATEMENT/01_INPUT_STATEMENTS.py
@@ -11,5 +11,7 @@
 
 a = input('Enter Name:') # DEFAULT INPUT IN A STRING FORMATE.
 num = eval(input('Enter Number:')) # IT WILL AUTOMATICALLY IDENTIFY THE TYPE OF DATA AND STORE IT.
+# Note : If we use eval() that time if we try to give a input in a string formate, then we need to write the string in a quotes ( ' ' ).
 
 num = int(input('Enter Any Number:')) # TYPECASTING
+
