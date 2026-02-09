7847e0dd22c21a5a21ef9b2188003ccd891f0ba8
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2026-02-09 18:17:26 +0530

    Update 02_SHALLOW_COPY.py

--- 06_COPY_OPERATION/02_SHALLOW_COPY.py
+++ 06_COPY_OPERATION/02_SHALLOW_COPY.py
@@ -18,3 +18,8 @@ b[0] = 10
 
 print(a) # OP: [1, [1, 20], 3]
 print(b) # OP: [10, [1, 20], 3]
+
+'''
+- The Nested collection stored in the same address.
+- So if we modify the value from nested collection, it will affect for both variables.
+'''
