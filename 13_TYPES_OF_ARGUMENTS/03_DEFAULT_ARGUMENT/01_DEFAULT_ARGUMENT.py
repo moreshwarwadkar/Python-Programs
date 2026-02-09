11f697c27e5556eca91519f608c47a38f428fdda
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2026-02-09 18:41:58 +0530

    Update 01_DEFAULT_ARGUMENT.py

--- 13_TYPES_OF_ARGUMENTS/03_DEFAULT_ARGUMENT/01_DEFAULT_ARGUMENT.py
+++ 13_TYPES_OF_ARGUMENTS/03_DEFAULT_ARGUMENT/01_DEFAULT_ARGUMENT.py
@@ -1,7 +1,10 @@
 # DEFAULT ARGUMENT
 
+# - Default Argument means passing any value to the variable in a function declaration section. 
+# - [ Not Passing Only Default Value. We Can Pass Any Value as a Default value to the variable ]
 def default(a=40, b=[], c=0j):
 
     print(a,b,c)
 
 default()
+
