750dfd407ed2a6b0074e45d7f78b3f217913facf
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2026-02-09 18:22:43 +0530

    Update 03_DEEP_COPY.py

--- 06_COPY_OPERATION/03_DEEP_COPY.py
+++ 06_COPY_OPERATION/03_DEEP_COPY.py
@@ -1,5 +1,14 @@
 # DEEP COPY
 
+'''
+* SYNTAX :
+
+  import copy
+  destination_var = copy.deepcopy(source_var)
+
+'''
+
+
 import copy
 
 a = [1,[10,20],3]
@@ -15,3 +24,4 @@ print(b) # OP: [10, [10, 20], 3]
 b[1][0] = 1
 print(a) # OP: [1, [10, 20], 3]
 print(b) # OP: [10, [1, 20], 3]
+
